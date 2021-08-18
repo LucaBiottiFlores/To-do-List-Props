f628db143f11e6c95c5b2e29caa40cc88cbbdcdd
Luca <luca.biotti.flores@gmail.com>
2021-08-17 22:02:33 -0400

    todo realizado, falta trabajar accesibilidad con paleta de colores

---
+++ src/components/Child.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="Child">
+    <div class="d-flex flex-column justify-content-center flex-wrap">
+      <h2 class="listTitle my-5">Lista</h2>
+      <div
+        class="
+          listContainer
+          d-flex
+          flex-column
+          justify-content-center
+          flex-wrap
+          px-5
+          mx-5
+        "
+      >
+        <ul class="list d-flex justify-content-center flex-column">
+          <li
+            class="listItem my-1 d-flex justify-content-start"
+            v-for="(item, $index) in items"
+            :key="$index"
+          >
+            {{ $index }}) {{ item }}
+            <button
+              @click="$emit('eliminar')"
+              class="liBtn ms-auto me-3"
+              type="button"
+            >
+              Eliminar
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data: () => ({}),
+
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.listTitle {
+  text-align: center;
+}
+.liBtn {
+  background: rgba(184, 233, 134, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+}
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="Parent">
+    <div class="container justify-content-center d-flex">
+      <div class="mt-5 pt-5">
+        <div class="d-flex justify-content-center">
+          <input
+            class="formInput px-3 mx-3"
+            placeholder="Ingrese una nueva tarea"
+            v-model="newItem"
+          />
+          <button @click="addItem" type="button" class="formBtn">Crear</button>
+        </div>
+        <Child :items="items" @eliminar="deleteItem"></Child>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  name: 'Parent',
+  data: () => ({
+    items: [],
+    newItem: ''
+  }),
+
+  components: {
+    Child
+  },
+
+  methods: {
+    addItem() {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+
+    deleteItem() {
+      this.items.splice(this.items.indexOf(this.newItem), 1)
+    }
+  }
+}
+</script>
+
+<style>
+.formInput {
+  width: 350px;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+}
+
+.formBtn {
+  width: 200px;
+  background: rgba(184, 233, 134, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+}
+
+.container {
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+}
+</style>
